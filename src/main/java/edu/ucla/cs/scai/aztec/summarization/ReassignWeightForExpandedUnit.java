c263c8f4975842d25ea0ffe9ae765000627bd816
xxlltm <xinxinh1993@gmail.com>
2016-09-21 19:37:44 -0700

    changes to subphrase expansion

---
+++ src/main/java/edu/ucla/cs/scai/aztec/summarization/ReassignWeightForExpandedUnit.java
@@ -0,0 +1,7 @@
+package edu.ucla.cs.scai.aztec.summarization;
+
+/**
+ * Created by Xinxin on 8/19/2016.
+ */
+public class ReassignWeightForExpandedUnit {
+}
---
+++ src/main/java/edu/ucla/cs/scai/aztec/utils/PrintObject.java
@@ -0,0 +1,7 @@
+package edu.ucla.cs.scai.aztec.utils;
+
+/**
+ * Created by Xinxin on 8/29/2016.
+ */
+public class PrinrObject {
+}
